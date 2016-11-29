734779c3b7a04371bf3cfaa1c5b5829b98baebd3
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-11-29 15:23:15 +0700

    Update Application

---
+++ src/main/java/web/App.java
@@ -0,0 +1,13 @@
+package web;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class App {
+
+  public static void main(String [ ] s) {
+    SpringApplication.run(App.class, s);
+  }
+
+}
+
