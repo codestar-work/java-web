6e1b585bee446a231a60b07e3d3bb313f24a42e5
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-11-29 18:08:19 +0700

    Add login mechanism

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -1,5 +1,7 @@
 package web;
+import java.util.*;
 import org.hibernate.*;
+import javax.servlet.http.*;
 import org.springframework.ui.*;
 import org.springframework.boot.*;
 import org.springframework.stereotype.*;
@@ -35,12 +37,47 @@ public class Main {
 		s.save(m);
 		s.flush();
 		s.close();
-		return "index";
+		return "redirect:/login";
 	}
 	
 	@RequestMapping("/login")
 	String showLoginPage() {
 		return "login";
+	}
+	@RequestMapping(method=RequestMethod.POST, value="/login")
+	String checkPassword(String email, String password, 
+			HttpSession session) {
+		boolean correct = false;
+		String data = encode(password);
+		Session database = factory.openSession();
+		Query query = database.createQuery(
+				"from Member where email = :e");
+		query.setParameter("e", email);
+		List list = query.list();
+		if (list.size() == 1) {
+			Member m = (Member)list.get(0);
+			if (m.password.equals(data)) {
+				correct = true;
+			}
+		}
+		
+		if (correct) {
+			Member m = (Member)list.get(0);
+			session.setAttribute("member", m);
+			return "redirect:/shop";
+		} else {
+			return "redirect:/login";
+		}
+	}
+	
+	@RequestMapping("/shop")
+	String showMemberShop(HttpSession session) {
+		Member m = (Member)session.getAttribute("member");
+		if (m == null) {
+			return "redirect:/login";
+		} else {
+			return "shop";
+		}
 	}
 
 	@Autowired
