65b358097c6d7e622ee5f33721f8059446f1d0dd
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-11-29 15:26:41 +0700

    Add Controller

--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -1,6 +1,15 @@
-
+package web;
+import org.springframework.ui.*;
+import org.springframework.boot.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class Main {
+
+  @RequestMapping("/test") @ResponseBody
+  int test() {
+    return 555;
+  }
 
 }
